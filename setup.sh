59ce76bd8a954427cfaa1157fd406c6d43622a44
Mohammed Jawabri <54251668+jxbt@users.noreply.github.com>
2024-06-07 02:45:03 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,32 +20,34 @@ ALTER ROLE postgresql SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE postgresql TO postgresql;
 EOF
 
-# Step 3: Install OpenJDK 21
-echo "Installing OpenJDK 21..."
-sudo apt install -y openjdk-21-jdk
+# Step 3: Install OpenJDK 11 Slim
+echo "Installing OpenJDK 11 Slim..."
+sudo apt install -y openjdk-11-jdk-headless
 
+# Step 4: Set Environment Variable
+echo "Setting environment variable..."
+export SPRING_PROFILES_ACTIVE=postgresql
 
-# Step 4: Set Up Your Application
+# Step 5: Set Up Your Application
 echo "Setting up application directories..."
 mkdir -p "$APP_DIR" "$BUILD_DIR"
 echo "Copying application files..."
 cp -r ./* "$APP_DIR/"
 
-# Step 5: Modify Configuration
+# Step 6: Modify Configuration
 echo "Modifying PostgreSQL configuration..."
-sed -i 's/localhost:5432/db:5432/' "$APP_DIR/src/main/resources/application-postgresql.properties"
+sed -i 's/localhost:5432/db:5432/' "$APP_DIR/src/main/resources/application.yaml"
 
-# Step 6: Build Your Application
+# Step 7: Build Your Application
 echo "Building application..."
 cd "$APP_DIR"
 ./gradlew --no-daemon clean build --stacktrace
-cp build/libs/java-spring-vuly-0.1.0.jar "$BUILD_DIR/"
+cp build/libs/java-spring-vuly-0.2.0.jar "$BUILD_DIR/"
 rm -Rf build/ "$APP_DIR" /root/.gradle/
 
-# Step 7: Run Your Application in the Background
+# Step 8: Run Your Application in the Background
 echo "Running application in the background..."
 cd "$BUILD_DIR"
-nohup java -Djava.security.egd=file:/dev/./urandom -jar java-spring-vuly-0.1.0.jar > app.log 2>&1 &
-sleep 10
+nohup java -Djava.security.egd=file:/dev/./urandom -jar java-spring-vuly-0.2.0.jar > app.log 2>&1 &
 
 echo "Application is running in the background. Check app.log for output."
